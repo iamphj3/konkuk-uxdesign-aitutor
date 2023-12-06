bb1f504a789910489bf1c8b19f72be9abfb89894
parkhyeonji <iamphj3@naver.com>
2023-12-07 02:39:12 +0900

    :lipstick: 채팅창 길이 늘리기 기능 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,15 +1,17 @@
-import BgAiTutor from "./bg_aitutor.svg?react";
-import IcContact from "./ic_contact.svg?react";
-import IcDetailArrow from "./ic_detail_arrow.svg?react";
-import IcEmpty from "./ic_empty.svg?react";
-import IcLeft from "./ic_left.svg?react";
-import IcLogo from "./ic_logo.svg?react";
-import IcMail from "./ic_mail.svg?react";
-import IcOffice from "./ic_office.svg?react";
-import IcPerson from "./ic_person.svg?react";
-import IcPhone from "./ic_phone.svg?react";
-import IcQuestion from "./ic_question.svg?react";
-import IcSuggestion from "./ic_suggestion.svg?react";
+import BgAiTutor from './bg_aitutor.svg?react';
+import IcCheckAfter from './ic_check_after.svg?react';
+import IcCheckBefore from './ic_check_before.svg?react';
+import IcContact from './ic_contact.svg?react';
+import IcDetailArrow from './ic_detail_arrow.svg?react';
+import IcEmpty from './ic_empty.svg?react';
+import IcLeft from './ic_left.svg?react';
+import IcLogo from './ic_logo.svg?react';
+import IcMail from './ic_mail.svg?react';
+import IcOffice from './ic_office.svg?react';
+import IcPerson from './ic_person.svg?react';
+import IcPhone from './ic_phone.svg?react';
+import IcQuestion from './ic_question.svg?react';
+import IcSuggestion from './ic_suggestion.svg';
 
 export {
   IcLeft,
@@ -24,4 +26,6 @@ export {
   IcOffice,
   IcPerson,
   IcPhone,
+  IcCheckAfter,
+  IcCheckBefore,
 };
--- src/components/AiTutor/AiTutor.tsx
+++ src/components/AiTutor/AiTutor.tsx
@@ -1,8 +1,16 @@
-import { styled } from "styled-components";
+import { useState } from 'react';
+import { styled } from 'styled-components';
 
-import BaseLayout from "../Common/BaseLayout";
+import { IcCheckAfter, IcCheckBefore } from '../../assets/icons';
+import BaseLayout from '../Common/BaseLayout';
 
 const AiTutor = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleHeight = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <BaseLayout
       headerName="AI Tutor"
@@ -15,7 +23,16 @@ const AiTutor = () => {
         </p>
       }
     >
-      <StAiTutor>
+      <StAiTutor $expanded={expanded}>
+        <StCheckboxLabel>
+          {expanded ? <StIcCheckAfter /> : <StIcCheckBefore />}
+          채팅창 길이 늘리기
+          <StCheckbox
+            type="checkbox"
+            checked={expanded}
+            onChange={toggleHeight}
+          />
+        </StCheckboxLabel>
         <iframe src="https://www.chatbase.co/chatbot-iframe/3t6nItqHc-BZCQGWHrypM" />
       </StAiTutor>
     </BaseLayout>
@@ -24,11 +41,53 @@ const AiTutor = () => {
 
 export default AiTutor;
 
-const StAiTutor = styled.div`
+const StIcCheckAfter = styled(IcCheckAfter)`
+  width: 100%;
+  height: auto;
+  max-width: 2.3rem;
+  max-height: 2.3rem;
+`;
+
+const StIcCheckBefore = styled(IcCheckBefore)`
+  width: 100%;
+  height: auto;
+  max-width: 2.3rem;
+  max-height: 2.3rem;
+`;
+
+const StCheckboxLabel = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 1rem;
+
+  height: 2.3rem;
+  max-height: 2.3rem;
+  margin-bottom: 1.7rem;
+  float: right;
+
+  color: #242331;
+  font-family: Open Sans;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 100%; /* 3rem */
+  text-transform: capitalize;
+`;
+
+const StCheckbox = styled.input`
+  display: none;
+`;
+
+const StAiTutor = styled.div<{ $expanded: boolean }>`
+  & > ${StCheckboxLabel} {
+    cursor: pointer;
+  }
   & > iframe {
     width: 100%;
     height: 100%;
-    min-height: 68rem;
+    /* min-height: 68rem; */
+    min-height: ${({ $expanded }) => ($expanded ? "136rem" : "68rem")};
 
     @media (max-width: 768px) {
       height: 350rem;
